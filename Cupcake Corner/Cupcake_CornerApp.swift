333b729643eba530a4e877bf85c9ee447022b036
simransdsu <simran5590@gmail.com>
2022-06-09 19:56:08 -0700

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Simran Preet Singh Narang on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
