9e072b8426fecf8a32fc817d5c83f8a1cd5d33f2
Simran Preet Singh <simran5590@gmail.com>
2022-06-12 15:08:22 -0700

    Preparing for checkout

--- Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/CheckoutView.swift
@@ -12,7 +12,27 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text("Hello Checkout View")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total is: \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order") {
+                    
+                }
+                .padding()
+            }
+        }.navigationTitle("Checkout")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
